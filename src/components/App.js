049ce94d37d1c338dbc1c10f42ed832f88a8e550
Shantanu <77541968+shantanu1998s@users.noreply.github.com>
2023-02-26 21:01:43 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import '../styles/App.css';
 const App = () => {
+  var time;
+  const handleDoubleClick = (event) => {
+    clearTimeout(time);
+    if (event.detail == 1) {
+      time = setTimeout(() => {
+        console.log("I was not double clicked")
+      }, 200)
 
-  const handleDoubleClick = (event) =>{
-  
+    } else if (event.detail == 2) {
+
+      console.log("I was double clicked")
+    }
   }
   return (
     <div id="main">
-      <button id="dblclick-btn">Double click me</button>
+      <button id="dblclick-btn" onClick={handleDoubleClick}>Double click me</button>
     </div>
   )
 }
